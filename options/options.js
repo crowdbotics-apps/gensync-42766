0000930a98bc07e71bae401d9a51650e910b5b03
Crowdbotics <team@crowdbotics.com>
2023-08-10 04:32:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gensync-42766",
+  url: "https://gensync-42766.botics.co",
+  api: "https://gensync-42766.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
